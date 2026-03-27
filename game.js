3d18184be1bd050bad42aeff44145b092a8b5c4b
pablito528 <119979546+pablito528@users.noreply.github.com>
2026-03-27 03:31:09 +0000

    menu funcional e outros assets

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
-/*import config from "./config.js";
+import config from "./config.js";
 import sceneMenu from "./sceneMenu.js";
-import scene1 from "./scene0.js";
-import scene0 from "./scene1.js";
+//import scene1 from "./scene1.js";
+import scene0 from "./scene0.js";
 
 class Game extends Phaser.Game {
   constructor() {
@@ -9,27 +9,27 @@ class Game extends Phaser.Game {
 
     this.scene.add("sceneMenu", sceneMenu);
     this.scene.add("scene0", scene0);
-    this.scene.add("scene1", scene1);
-    this.scene.start("scene0");
-  }
-}
-
-window.onload = () => {
-  window.game = new Game();
-};*/
-
-import config from "./config.js";
-import scene0 from "./scene0.js";
-
-class Game extends Phaser.Game {
-  constructor() {
-    super(config);
-
-    this.scene.add("scene0", scene0);
-    this.scene.start("scene0");
+    // this.scene.add("scene1", scene1);
+    this.scene.start("sceneMenu");
   }
 }
 
 window.onload = () => {
   window.game = new Game();
 };
+
+// import config from "./config.js";
+// import scene0 from "./scene0.js";
+
+// class Game extends Phaser.Game {
+//   constructor() {
+//     super(config);
+
+//     this.scene.add("scene0", scene0);
+//     this.scene.start("scene0");
+//   }
+// }
+
+// window.onload = () => {
+//   window.game = new Game();
+// };
--- sceneMenu.js
+++ sceneMenu.js
@@ -1,44 +1,76 @@
-class SceneMenu extends Phaser.Scene {
+export default class SceneMenu extends Phaser.Scene {
   constructor() {
     super("sceneMenu");
   }
 
   preload() {
-    this.load.image("atirador", "assets/botao_atirador.png");
-    this.load.image("piloto", "assets/botão_piloto.png");
-    this.load.image("dudecapa", "assets/borda_menu.png");
-    
+    this.load.plugin(
+      "rexvirtualjoystickplugin",
+      "../rexvirtualjoystickplugin.min.js",
+      true,
+    );
+    this.load.setPath("assets/");
+//Menu  
+    this.load.image("atirador", "botao_atirador.png");
+    this.load.image("piloto", "botao_piloto.png");
+    this.load.image("borda", "borda_menu.png");
+    this.load.image("fundo", "fundo.png");
+    this.load.audio("musica", "menusong.mp3");
+    this.load.audio("som", "lazer.mp3");
 
 
     //scene0
-    this.load.image("star", "assets/star.png");
-    this.load.image("starVermelha", "assets/starVermelha.png");
-    this.load.image("starCinza", "assets/starCinza.png");
-    this.load.image("chao", "assets/chao2.png");
-    this.load.image("rock", "assets/rocha.png");
-    this.load.image("et1", "assets/inimigo.png");
-    this.load.image("et2", "assets/inimigoCobra.png");
+    this.load.image("mapf1", "mapf1.png");
+    this.load.image("star", "star.png");
+    this.load.image("asteroideum", "asteroideum.png");
+    this.load.image("startf1", "startf1.png");
+    this.load.audio("songf1", "songf1.mp3");
+    this.load.image("et1", "enemigo1.png");
 
-    this.load.spritesheet("enemigo1", "assets/enemigo1.png", {
+    this.load.spritesheet("estrelas", "estrelas_sprite_shit.png", {
+      frameWidth: 800,
+      frameHeight: 450,
+    });
+    
+    this.load.spritesheet("combustivel", "combustivel.png", {
       frameWidth: 64,
       frameHeight: 64,
     });
 
-    this.load.audio("musica", "assets/menusong.mp3");
-    this.load.audio("som", "assets/lazer.mp3");
+    this.load.spritesheet("uispritesheet", "uispritesheet.png", {
+      frameWidth: 800,
+      frameHeight: 450,
+    });
+
+    this.load.spritesheet("button", "button.png", {
+      frameWidth: 32,
+      frameHeight: 32,
+    });
+  }
+   
 
     // //Ende Game
     // this.load.image("botao_Voltar", "assets/voltar.png");
-  }
 
   create() {
+  this.music = this.sound.add("musica", { loop: true });
+
+  // Espera o primeiro clique/toque para iniciar a música
+  this.input.once("pointerdown", () => {
+    this.music.play();
+  });
+
+    this.anims.create({
+      frames: this.anims.generateFrameNumbers("estrelas", { start: 0, end: 14 }),
+      frameRate: 24,
+      repeat: -1
+    });
+
     this.add.image(0, 0, "fundo").setOrigin(0, 0);
-    let btnPlay = this.add.image(135, 200, "btnPlay").setOrigin(0, 0);
+    let btnPlay = this.add.image(320, 230, "piloto").setOrigin(0, 0);
     btnPlay.setInteractive();
 
-    this.add.image(320, 230, "dudecapa").setOrigin(0, 0);
-
     //Adicionar o clique do botao
-    btnPlay.on("pointerdown", () => this.scene.start("PlayGame"));
+    btnPlay.on("pointerdown", () => this.scene.start("scene0"));
   }
 }
